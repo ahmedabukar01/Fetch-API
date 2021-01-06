f08ab1ac0608438daac27427bb1a3eca85dfc6d1
ahmed abukar <ahmedlearn01@gmail.com>
2021-01-06 13:48:45 +0300

    done Api

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const btnText = document.querySelector('#getText');
 const btnUsers = document.getElementById("getusers");
 const btnPosts = document.querySelector('#getposts');
+const post = document.querySelector('#post');
 const output = document.getElementById("output");
 
 btnText.addEventListener('click',getText);
 btnUsers.addEventListener('click',getUsers);
 btnPosts.addEventListener('click',getPosts);
+post.addEventListener('submit',postForm);
 
 function getText(){
     fetch('simple.txt')
@@ -41,11 +43,32 @@ function getPosts(){
         let iio = '<h2>Posts</h2>';
         posts.forEach(post=>{
             iio +=`
+            <div>
             <h3>${post.title}</h3>
             <p>${post.body}</p>
+            </div>
             `
         });
         output.innerHTML+=iio;
     })
     .catch(err=>console.log("fetch error",err.message));
+}
+
+// post
+
+function postForm(e){
+    e.preventDefault();
+    let title = document.getElementById('title').value;
+    let body = document.getElementById('body').value;
+    
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+        method:'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'content-type': 'application/json'
+        },
+        body:JSON.stringify({title:title, body:body})
+    })
+    .then(res=>res.json())
+    .then(data=>console.log(data));
 }
