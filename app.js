9e6689b769c7fce0a45d5b142da10cb82f75944b
ahmed abukar <ahmedlearn01@gmail.com>
2021-01-06 12:00:23 +0300

    here we go

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const btnText = document.querySelector('#getText');
 const btnUsers = document.getElementById("getusers");
+const btnPosts = document.querySelector('#getposts');
 const output = document.getElementById("output");
 
 btnText.addEventListener('click',getText);
 btnUsers.addEventListener('click',getUsers);
+btnPosts.addEventListener('click',getPosts);
 
 function getText(){
     fetch('simple.txt')
@@ -30,4 +32,20 @@ function getUsers(){
         output.innerHTML +=outputData;
     })
     .catch(err=>console.log(err));
+}
+
+function getPosts(){
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(data=>data.json())
+    .then(posts=>{
+        let iio = '<h2>Posts</h2>';
+        posts.forEach(post=>{
+            iio +=`
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            `
+        });
+        output.innerHTML+=iio;
+    })
+    .catch(err=>console.log("fetch error",err.message));
 }
