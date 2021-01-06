738e6ecaaa5e78fb3803d135b5b3c308533c6e0f
ahmed abukar <ahmedlearn01@gmail.com>
2021-01-06 11:15:03 +0300

    json data

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-const btn = document.querySelector('#getText');
-btn.addEventListener('click',getText);
+const btnText = document.querySelector('#getText');
+const btnUsers = document.getElementById("getusers");
+const output = document.getElementById("output");
+
+btnText.addEventListener('click',getText);
+btnUsers.addEventListener('click',getUsers);
+
 function getText(){
     fetch('simple.txt')
+    .then(text=> text.text())
     .then(text=>{
-        return text.text();
+        output.innerHTML=text;
     })
-    .then(text=>console.log(text));
+    .catch(err=>console.log(err));
+}
+function getUsers(){
+    fetch('data.json')
+    .then(user=>user.json())
+    .then(user=>{
+        let outputData = '<h2>Users Data </h2>';
+        user.forEach(res=>{
+            outputData+= `
+            <ul>
+                <li>ID: ${res.id}</li>
+                <li>Name: ${res.name}</li>
+                <li>Email: ${res.email}</li>
+            </ul>
+            `
+        });
+        output.innerHTML +=outputData;
+    })
+    .catch(err=>console.log(err));
 }
